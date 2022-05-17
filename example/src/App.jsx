e609a13036618fe3d23d5fddbd4ba1d6479bdeaa
Anna-Lena Schwarzkopf <annalena.schwarzkopf@goto.com>
2022-05-17 18:26:12 +0200

    add inline styling

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 
 import { SmartBanner } from 'smartbanner-react'
-import 'smartbanner-react/dist/index.css'
 
 const App = () => {
-  return <SmartBanner src='' author='me' description='describe me' buttonLink='google.com' buttonText='google' />
+  return (
+    <SmartBanner
+      src='https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_1280.jpg'
+      name='MyApp'
+      author='lena.codes'
+      description='GET - On the Play Store'
+      buttonLink='https://lena.codes'
+      buttonText='View'
+    />
+  )
 }
 
 export default App
