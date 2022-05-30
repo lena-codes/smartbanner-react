9e36f49f09922c4d9453f36f0faf234f12c2eb92
Anna-Lena Schwarzkopf <zckrbrt@gmail.com>
2022-05-30 14:41:53 +0200

    add index.d.ts to repo
    oopsy was gitignored before

---
+++ index.d.ts
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+// Type definitions for @lena-codes/smartbanner-react
+// Project: @lena-codes/smartbanner-react
+// Definitions by: lena-codes <https://lena.codes>
+
+import React from 'react'
+import { SmartBannerProps } from '.'
+import { CloseIconProps } from './src/Icon/CloseIcon'
+
+declare const SmartBanner: React.FC<SmartBannerProps>
+declare const CloseIcon: React.FC<CloseIconProps>
+
+export as namespace SmartBanner
